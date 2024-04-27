e9008766690668d1e5bc2ea5bfd2d0c139db4b4d
liulei <193002818@qq.com>
2024-04-27 20:42:25 +0800

    update TestFileHeader.java

--- ideaIC-2023.3.6.win-extra/all_config/.IdeaIC/config/fileTemplates/includes/TestFileHeader.java
+++ ideaIC-2023.3.6.win-extra/all_config/.IdeaIC/config/fileTemplates/includes/TestFileHeader.java
@@ -1,6 +1,9 @@
 /**
-* Tests for {@link ${NAME}}.
-*
+## description:
+## when generate TEST class, generate the class javadoc to link the the source class.
+#set($classNameSuffix = "Test")
+
+* Tests for {@link #if (${NAME.endsWith($classNameSuffix)})#set($endIndex = ${NAME.length()} - ${classNameSuffix.length()})${NAME.substring(0, $endIndex)}#else${NAME}#end}.
 * @author lei.liu
 * @since ${YEAR}-${MONTH}-${DAY}
 */
